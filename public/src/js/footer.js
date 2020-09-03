e1548c1ad2c640ca1d0d77693aa576066282a7d6
Sabri <sabri.sonmez@bmwgroup.com>
2020-09-03 16:31:38 -0400

    footer change

--- public/src/js/footer.js
+++ public/src/js/footer.js
@@ -1,59 +1,15 @@
-function randomRange(max, min) {
-    return (Math.floor(Math.random() * (+max - +min)) + +min); 
-  }
-  function getRanDeci(min, max) {
-    return Math.random() * (max - min) + min;
-  }
-  
-  
-  function drawBubbles() {
-    var w = window.innerWidth;
-    var bubbles;
-    document.getElementById('gooey-anim').innerHTML = '';
-  
-  
-    if(w >= 1000) {
-      bubbles = 80;
-    } else
-      if(w >= 500) {
-        bubbles = 40;
-      } else if (w >= 0) 
-      {
-        bubbles = 10;
-      }
-    console.log(bubbles);
-  
-  
-    for (i = 0; i <= bubbles; i++) {
-      var iDiv = document.createElement('div');
-      var size = randomRange(120, 50);
-  
-      iDiv.id = 'blubb' + i;
-      iDiv.className = 'blubb';
-      iDiv.style.animationDelay = getRanDeci(0,5)+"s";
-      iDiv.style.animationDuration = getRanDeci(3,4)+"s";
-      iDiv.style.backgroundColor = "#00897b";
-      iDiv.style.width = size + "px";
-      iDiv.style.height = size + "px";
-      iDiv.style.left = randomRange(w, 0) - (size/2) + "px";
-      iDiv.style.top = 0;
-  
-      document.getElementById('gooey-anim').appendChild(iDiv);
-  
+function generateBalls() {
+    for (var i = 0; i < Math.floor(window.innerWidth/20); i++) {
+      $(".gooey-animations").append(`
+      <div class="ball"></div>
+    `);
+      var colors = ['#b2dfdb','#00796b'];
+      $(".ball").eq(i).css({"bottom":"0px","left":Math.random()*window.innerWidth-100,"animation-delay":Math.random()*5+"s","transform":"translateY("+Math.random()*10+"px)","background-color":colors[i%2]});
     }
   }
+  generateBalls();
   
-  
-  window.onresize = function(event) {
-    drawBubbles();
-  };
-
-          
-    document.onreadystatechange = () => {
-      if (document.readyState === 'complete') {
-        
-            
-        
-          drawBubbles();
-      }
-    };
+  window.addEventListener('resize', function(e) {
+    $(".gooey-animations .ball").remove();
+    generateBalls();
+  })
