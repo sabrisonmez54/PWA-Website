71a6ae467b5eee7a0cf8bfda33ff05b2a5440ec2
Sabri <sabri.sonmez@macaulay.cuny.edu>
2020-09-02 22:56:03 -0400

    first commit

---
+++ public/src/js/pwa.js
@@ -0,0 +1,27 @@
+var installButton = document.querySelector('#install');
+
+
+function installButtonClickHandler() {
+
+    if (deferredPrompt) {
+        deferredPrompt.prompt();
+
+        deferredPrompt.userChoice.then(function (choiceResult) {
+            console.log(choiceResult.outcome);
+
+            if (choiceResult.outcome === 'dismissed') {
+                console.log('User cancelled installation');
+            } else {
+                console.log('User added to home screen');
+            }
+        });
+
+        deferredPrompt = null;
+    }
+}
+
+
+
+installButton.addEventListener('click', installButtonClickHandler);
+
+
