ac89898d02b4df2e545ff611993bb1d2ee2ea2e2
Sabri <sabri.sonmez@bmwgroup.com>
2020-09-04 17:02:11 -0400

    resume

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -9,6 +9,17 @@ var elektra = document.querySelector('#elektra')
 elektra.addEventListener('click', elektraClick)
 var bmw = document.querySelector('#bmw')
 bmw.addEventListener('click', bmwClick)
+var set = document.querySelector('#set')
+set.addEventListener('click', setClick)
+var smack = document.querySelector('#smack')
+smack.addEventListener('click', smackClick)
+var movie = document.querySelector('#movie')
+movie.addEventListener('click', movieClick)
+var java = document.querySelector('#java')
+java.addEventListener('click', javaClick)
+var echoar = document.querySelector('#echoar')
+echoar.addEventListener('click', echoarClick)
+
 
 function pwaClick() {
 
@@ -19,7 +30,7 @@ function pwaClick() {
 
 function strongarmClick() {
 
-    window.location.assign("/sat");
+    window.location.assign("/strongarm");
     //openCreatePostModal()
 }
 
@@ -41,3 +52,28 @@ function bmwClick() {
     //openCreatePostModal()
 }
 
+function setClick() {
+
+    window.location.assign("/set");
+    //openCreatePostModal()
+}
+function smackClick() {
+
+    window.location.assign("/smack");
+    //openCreatePostModal()
+}
+function movieClick() {
+
+    window.location.assign("/movieapp");
+    //openCreatePostModal()
+}
+function javaClick() {
+
+    window.location.assign("/java");
+    //openCreatePostModal()
+}
+function echoarClick() {
+
+    window.location.assign("/echoar");
+    //openCreatePostModal()
+}
